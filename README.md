a987110f27637e89ec93529943a68d83080846b1
Camille Huang <camille_huang@berkeley.edu>
2022-05-12 19:45:10 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Video Game Genre Classification by Global Sales Data, Game Platform, and ESRB Content¶
+Angela Hou and Camille Huang
+
+## Abstract
+The video game community is quickly growing to become a multi-billion dollar industry worldwide. For new developers and publishers seeking to enter an expanding market, it would be useful to know which video game genres, platforms, and ESRB ratings are the most popular in a certain region. On the consumer side, knowing the genre of enjoyable games can help with the search for more interesting or fun content. In this paper, we seek to discover trends between genre, platform, ESRB rating, and sales in different regions, and then seek to classify a video game genre based on sales data, as well as platform and ESRB rating. The results show that our classification reflects the limitations of current genre classification, and more improvement must be made for this classification to perform sufficiently.
---
+++ scraping.py
@@ -0,0 +1,64 @@
+"""
+A script for scraping data from ESRB website
+!!! Make sure these packages and software are installed:
+- beautifulsoup (pip install beautifulsoup4)
+- selenium (pip install -U selenium)
+- webdriver_manager (pip install webdriver_manager --user)
+- google chrome (https://www.google.com/chrome/)
+
+Author: Jie Li
+Date created: Mar 10, 2021
+"""
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+import re
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+within_tag = r"<\s*[^>]*>(.*?)<\/.*>"
+img_match = r"images/(.*?).svg"
+
+def scrape_single(driver, game_name):
+    data = []
+    website = f"https://www.esrb.org/search/?searchKeyword={game_name}&platform=Nintendo%20Switch%2CNintendo%203DS%2CWii%20U%2CPlayStation%205%2CPlayStation%204%2CPlayStation%203%2CXbox%20Series%2CXbox%20One%2CXbox%20360%2CStadia%2CPC%2COther&rating=E%2CE10%2B%2CT%2CM%2CAO&descriptor=All%20Content&pg=1&searchType=All"
+    driver.get(website)
+    for i in range(6):
+        content = driver.page_source
+        soup = BeautifulSoup(content)
+        matches = soup.findAll("div", {"class": "game"})
+        if len(matches) == 0:
+            # maybe the search result has not yet loaded: wait 0.5s and try again
+            time.sleep(0.5)
+            continue
+        for single_game_div in matches:
+            game_name = single_game_div.findAll("h2")[0].findAll("a")[0]
+            game_name = re.findall(within_tag, str(game_name))[0]
+            platforms = single_game_div.findAll("div",{"class":"platforms"})[0]
+            platforms = re.findall(within_tag, str(platforms))[0]
+            content_descriptors = single_game_div.findAll("td")[1]
+            content_descriptors = re.findall(within_tag, str(content_descriptors))[0]
+            rating = single_game_div.findAll("img")[0]
+            rating = re.findall(img_match, str(rating))[0]
+            data.append({"game_name": game_name, "platforms": platforms, "content_descriptors": content_descriptors, "rating": rating})
+        df = pd.DataFrame(data)
+        return df
+
+
+vgsales = pd.read_csv('vgsales.csv')
+
+#df = pd.DataFrame()
+
+for i in range(16597, len(vgsales['Name'])):
+    #df = pd.concat([df, scrape_single(driver, vgsales['Name'][i])], ignore_index=True)
+    df = scrape_single(driver, vgsales['Name'][i])
+
+    if df is not None:
+        with open('esrb_ratings.csv', 'a') as f:
+            df.to_csv(f, header=f.tell()==0)
+
+    print(i)
+        
